1cd2bc01c463ca25d28f8c8f4638a8b4c3934d38
didwlwd <didehdals321@naver.com>
2025-04-01 16:45:58 +0900

    add : domain.vo

---
+++ src/main/java/com/kh/diamelo/domian/vo/Product_Category.java
@@ -0,0 +1,13 @@
+package com.kh.diamelo.domian.vo;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
+@NoArgsConstructor
+public class Product_Category {
+    private int procategory_No; //제품 카테고리 번호
+    private String pro_Category; //제품 카테고리명 (화장품 종류)
+}
