555023bfba75396383b2f73a0634bd7f98bbe366
didwlwd <didehdals321@naver.com>
2025-04-03 18:00:37 +0900

    add : boardUpdate , insertReply

--- src/main/java/com/kh/diamelo/domain/vo/Reply.java
+++ src/main/java/com/kh/diamelo/domain/vo/Reply.java
@@ -1,5 +1,6 @@
 package com.kh.diamelo.domain.vo;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
@@ -11,6 +12,7 @@ import lombok.*;
 public class Reply {
     @JsonProperty("reply_no")
     private int repNo; //댓글 번호
+    @JsonIgnore
     private int postId; //게시글 번호
     @JsonProperty("reply_content")
     private String content; //내용
@@ -18,6 +20,8 @@ public class Reply {
     private String writer; //작성자
     @JsonProperty("create_date")
     private String createDate; //작성일자
+
     private String updateDate; //수정일자
+
     private String replyDeleted; //삭제됨 Y, 삭제안됨 N
 }
